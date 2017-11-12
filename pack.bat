c4d8c523d93370379ea9260c3b8d9b57938f13b1
matarillo <kentaro.inomata@gmail.com>
2017-11-11 17:28:43 -0800

    Add Nemerle.Compiler.Utils.Unofficial

--- pack.bat
+++ pack.bat
@@ -2,3 +2,4 @@ if not exist out mkdir out
 nuget\nuget4.3.0.exe pack nuget\Nemerle.Unofficial.nuspec -OutputDirectory out
 nuget\nuget4.3.0.exe pack nuget\Nemerle.Compiler.Unofficial.nuspec -OutputDirectory out
 nuget\nuget4.3.0.exe pack nuget\Nemerle.Macros.Unofficial.nuspec -OutputDirectory out
+nuget\nuget4.3.0.exe pack nuget\Nemerle.Compiler.Utils.Unofficial.nuspec -OutputDirectory out
--- push.bat
+++ push.bat
@@ -2,3 +2,4 @@
 nuget\nuget4.3.0.exe push out\Nemerle.Unofficial.1.2.547.nupkg -Source https://www.nuget.org/api/v2/package
 nuget\nuget4.3.0.exe push out\Nemerle.Compiler.Unofficial.1.2.547.nupkg -Source https://www.nuget.org/api/v2/package
 nuget\nuget4.3.0.exe push out\Nemerle.Macros.Unofficial.1.2.547.nupkg -Source https://www.nuget.org/api/v2/package
+nuget\nuget4.3.0.exe push out\Nemerle.Compiler.Utils.Unofficial.1.2.547.nupkg -Source https://www.nuget.org/api/v2/package
